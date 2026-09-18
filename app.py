cdcb9695cb1156a5bbe49fc061cfe483dfaee02d
Lucas Santos de Oliveira <lucas.santos003@outlook.com>
2026-09-17 21:46:19 -0300

    Rotas e paginas inicias do sistema em funcionamento

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def pagina_inicial():
+    return render_template('index.html')
+
+@app.route('/listar')
+def listar():
+    return render_template('usuarios/listar.html')
+
+@app.route('/form')
+def form():
+    return render_template('usuarios/form.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
